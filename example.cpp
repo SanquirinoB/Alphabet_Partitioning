324f0fd15c419c5c382e71becdc102900904b4b5
SanquirinoB <fernanda.sanchirico@ug.uchile.cl>
2021-09-10 16:13:07 -0300

    Funciona, pero la clase cero tiene 2 elementos

--- example.cpp
+++ example.cpp
@@ -43,30 +43,32 @@ Alphabet_Partitioning::Alphabet_Partitioning(string S)
     int sigma_size = 95;
     // Definimos e inicializamos nuestro arreglo de caracteres y freciencias
     vector<pair<int, int>> F;
-    for(int i = 0; i < sigma_size; i++)
-    {
-        F.push_back(make_pair(0, i));
-    }
+    for(int i = 0; i < sigma_size; i++) F.push_back(make_pair(0, i));
     // Para cada caracter en el alfabeto, computamos su frecuencia dentro de S
-    for(int i = 0; i < n; i++)
-    {
-        F[to_int(S[i])].first += 1;
-    }
+    for(int i = 0; i < n; i++) F[to_int(S[i])].first += 1;
     // Ordenamos el alfabeto por frecuencia en orden descendente
     sort(F.rbegin(), F.rend());
     // Alocamos el arreglo con el id de las clases
-    string C[sigma_size];
+    char C[sigma_size];
     // Almacenamos el tamanio de cada clase
-    int* N[int(log(sigma_size))];
+    int N[int(log(sigma_size))];
     int ln_sigma = floor(log(sigma_size));
 
-    for(int l = 0; l < ln_sigma; l++)
+    for(int l = 0; l < ln_sigma; l++) N[l] = 0;
+    int l;
+    for(int j = 1; j < sigma_size; j++)
     {
-        N[l] = 0;
+        l = floor(log(j));
+        C[to_int(F[j].second)] = to_char(l);
+        std::cout << "A la clase " << l << endl;
+        std::cout << "le agregamos la frecuencia " << F[j].first << endl;
+        std::cout << "del caracter " << to_char(F[j].second) << endl;
+        N[l] += F[j].first;
     }
+
     std::cout << sigma_size << ln_sigma << endl;
     for(int i = 0; i < ln_sigma; i++){
-        std::cout << N[i] << ln_sigma << endl;
+        std::cout << N[i] << i << endl;
     }
     
     
